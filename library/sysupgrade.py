94c86c410094dc4bb1533d70cffa68084786b4cc
jcmdln <jcmdln@gmail.com>
2019-11-07 18:57:02 -0500

    cleanup pass on sysupgrade behavior

--- library/sysupgrade.py
+++ library/sysupgrade.py
@@ -9,145 +9,61 @@
 from __future__ import (absolute_import)
 from ansible.module_utils.basic import AnsibleModule
 
-ANSIBLE_METADATA = {
-    'metadata_version': '1.1',
-    'status': ['preview'],
-    'supported_by': 'community'
-}
 
-DOCUMENTATION = '''
----
-module: sysupgrade
-short_description: Upgrade OpenBSD to the next release or snapshot
-version_added: "2.9"
+def upgrade(r, module, Branch, Force):
+    if Branch == 'release':
+        r['command'] = "%s -r" % (r['command'])
+    elif Branch == 'snapshot':
+        r['command'] = "%s -s" % (r['command'])
 
-description:
-    - Use the sysupgrade(8) utility to upgrade a system to the next
-      release or the latest snapshot for OpenBSD 6.6 or later.
-
-options:
-    upgrade:
-        description:
-            - Upgrade to the next release or latest snapshot.
-        type: bool
-        default: false
-        required: true
-    force:
-        description:
-            - Force an already applied upgrade.  This option has no
-              effect on releases.
-        type: bool
-        default: false
-        required: false
-    keep:
-        description:
-            - Keep the files in /home/_sysupgrade
-        type: bool
-        default: false
-        required: false
-
-author:
-    - Johnathan C Maudlin (@jcmdln)
-'''
-
-EXAMPLES = '''
-- name: Upgrade to latest release or snapshot
-  when:
-    - ansible_distribution == 'OpenBSD'
-    - ansible_distribution_version >= '6.5' and ansible_distribution_release == 'current' or
-      ansible_distribution_version >= '6.6'
-  register: result
-  sysupgrade:
-    upgrade: yes
-
-- name: Reboot if upgrade performed
-  when:
-    - 'result is succeeded'
-    - result.changed == true
-  reboot:
-'''
-
-RETURN = '''
-changed:
-    description: A change on the host was reported
-    returned: always
-    type: bool
-command:
-    description: The command and arguments that were used
-    returned: always
-    type: str
-msg:
-    description: The message returned by the command
-    returned: always
-    type: str
-rc:
-    description: The command return code (0 means success)
-    returned: always
-    type: int
-stderr:
-    description: sysupgrade standard error
-    returned: always
-    type: str
-stdout:
-    description: sysupgrade standard output
-    returned: always
-    type: str
-'''
-
-
-def upgrade(r, module, force, keep):
-    if force:
+    if Force:
         r['command'] = "%s -f" % (r['command'])
-    if keep:
-        r['command'] = "%s -k" % (r['command'])
 
     r['rc'], r['stdout'], r['stderr'] = module.run_command(
         r['command'], check_rc=False
     )
 
     if 'ftp: Error retrieving file: 404 Not Found' in r['stderr']:
-        # This signifies that no later release is available, so catch
-        # this error and propagate the details.
+        # This signifies that no later release is available, which
+        # isn't really an error.
         r['rc'] = 0
         r['msg'] = 'already on the latest available release'
-    elif r['rc'] != 0:
+    elif r['rc'] > 0:
         r['changed'] = False
         r['msg'] = 'received a non-zero exit code'
     elif 'Already on latest' in r['stdout']:
         r['changed'] = False
         r['msg'] = 'no action required'
+    elif 'Will upgrade on next reboot' in r['stdout']:
+        r['changed'] = True
+        r['reboot'] = True
+        r['msg'] = 'Upgrade prepared successfully'
     else:
-        if 'Will upgrade on next reboot' in r['stdout']:
-            r['changed'] = True
-            r['msg'] = 'Upgrade prepared successfully'
-        else:
-            r['changed'] = True
-            r['msg'] = "something isn't right"
+        r['changed'] = True
+        r['msg'] = "something isn't right"
 
     return r
 
 
 def main():
-    module = AnsibleModule(
-        argument_spec={
-            'upgrade': {
-                'type': 'bool',
-                'required': True,
-            },
-            'force': {
-                'type': 'bool',
-                'default': False,
-            },
-            'keep': {
-                'type': 'bool',
-                'default': False,
-            },
+    module_args = {
+        'branch': {
+            'type': 'str',
+            'choices': ['auto', 'release', 'snapshot'],
         },
+        'force': {
+            'type': 'bool',
+            'default': False,
+        },
+    }
 
-        supports_check_mode=True
+    module = AnsibleModule(
+        argument_spec = module_args,
+        required_one_of=[['branch']],
+        supports_check_mode = True,
     )
 
-    r = {
+    result = {
         'changed': False,
         'command': '/usr/sbin/sysupgrade -n',
         'msg': 'no action performed',
@@ -157,23 +73,19 @@ def main():
         'stdout': '',
     }
 
-    p = module.params
+    Branch = module.params['branch']
+    Force = module.params['force']
 
-    if p['upgrade'] in ['yes', True]:
-        p['upgrade'] = True
+    if Force in ['yes', True]:
+        Force = True
 
-        if p['force'] in ['yes', True]:
-            p['force'] = True
+    if Branch in ['auto', 'release', 'snapshot']:
+        result = upgrade(result, module, Branch, Force)
 
-        if p['keep'] in ['yes', True]:
-            p['keep'] = True
-
-        r = upgrade(r, module, p['force'], p['keep'])
-
-    if r['rc'] == 1:
-        module.fail_json(**r)
+    if result['rc'] > 0:
+        module.fail_json(**result)
     else:
-        module.exit_json(**r)
+        module.exit_json(**result)
 
 
 if __name__ == '__main__':
