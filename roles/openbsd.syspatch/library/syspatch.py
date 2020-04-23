0a121f77904e2db8dbbd33cc6f59b34898f2d8c1
jcmdln <jcmdln@gmail.com>
2020-04-22 22:33:45 -0400

    cleanup pass

--- roles/openbsd.syspatch/library/syspatch.py
+++ roles/openbsd.syspatch/library/syspatch.py
@@ -6,124 +6,102 @@
 # GNU General Public License v3.0+ (see COPYING or
 # https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from __future__ import (absolute_import)
+from __future__ import absolute_import
 from ansible.module_utils.basic import AnsibleModule
 
 
-def syspatch_apply(r, module):
-    r['rc'], r['stdout'], r['stderr'] = module.run_command(
-        r['command'], check_rc=False
-    )
+def syspatch_apply(r, m):
+    r["rc"], r["stdout"], r["stderr"] = m.run_command(
+        r["command"], check_rc=False)
 
-    if r['rc'] != 0:
-        r['msg'] = 'received a non-zero exit code'
-        r['rc'] = 1
-    elif len(r['stdout']) < 1:
-        r['msg'] = 'nothing to do'
+    if r["rc"] != 0:
+        r["msg"] = "received a non-zero exit code"
+        r["rc"] = 1
+    elif not r["stdout"]:
+        r["msg"] = "nothing to do"
     else:
-        r['msg'] = 'patches applied, probably'
-        r['changed'] = True
+        r["msg"] = "patches applied, probably"
+        r["changed"] = True
 
     return r
 
 
-def syspatch_list(r, module, List):
-    if List == 'available':
-        r['command'] = "%s -c" % (r['command'])
+def syspatch_list(r, m):
+    if m.params["List"] == "available":
+        r["command"] = "%s -c" % (r["command"])
 
-    if List == 'installed':
-        r['command'] = "%s -l" % (r['command'])
+    if m.params["List"] == "installed":
+        r["command"] = "%s -l" % (r["command"])
 
-    r['rc'], r['stdout'], r['stderr'] = module.run_command(
-        r['command'],
-        check_rc=False
-    )
+    r["rc"], r["stdout"], r["stderr"] = m.run_command(
+        r["command"], check_rc=False)
 
-    if r['rc'] != 0:
-        r['msg'] = 'received a non-zero exit code'
-        r['rc'] = 1
-    elif len(r['stdout']) < 1:
-        r['msg'] = 'nothing to do'
+    if r["rc"] != 0:
+        r["msg"] = "received a non-zero exit code"
+        r["rc"] = 1
+    elif len(r["stdout"]) < 1:
+        r["msg"] = "nothing to do"
     else:
-        r['msg'] = 'list of patches returned'
+        r["msg"] = "list of patches returned"
 
     return r
 
 
-def syspatch_revert(r, module, Revert):
-    if Revert == 'all':
-        r['command'] = "%s -R" % (r['command'])
+def syspatch_revert(r, m):
+    if m.params["revert"] == "all":
+        r["command"] = "%s -R" % (r["command"])
 
-    if Revert == 'latest':
-        r['command'] = "%s -r" % (r['command'])
+    if m.params["revert"] == "latest":
+        r["command"] = "%s -r" % (r["command"])
 
-    r['rc'], r['stdout'], r['stderr'] = module.run_command(
-        r['command'],
-        check_rc=False
-    )
+    r["rc"], r["stdout"], r["stderr"] = m.run_command(
+        r["command"], check_rc=False)
 
-    if r['rc'] != 0:
-        r['msg'] = 'received a non-zero exit code'
-        r['rc'] = 1
-    elif len(r['stdout']) < 1:
-        r['msg'] = 'nothing to do'
+    if r["rc"] != 0:
+        r["msg"] = "received a non-zero exit code"
+        r["rc"] = 1
+    elif len(r["stdout"]) < 1:
+        r["msg"] = "nothing to do"
     else:
-        r['msg'] = 'patches reverted, probably'
-        r['changed'] = True
+        r["msg"] = "patches reverted, probably"
+        r["changed"] = True
 
     return r
 
 
 def main():
-    module_args = {
-        'apply': {
-            'type': 'bool',
-            'default': False,
-        },
-        'list': {
-            'type': 'str',
-            'choices': ['available', 'installed']
-        },
-        'revert': {
-            'type': 'str',
-            'choices': ['all', 'latest']
-        },
-    }
-
     module = AnsibleModule(
-        argument_spec=module_args,
-        required_one_of=[['apply', 'list', 'revert']],
-        mutually_exclusive=[['apply', 'list', 'revert']],
-        supports_check_mode=False
+        argument_spec={
+            "apply": {"type": "bool", "default": False},
+            "list": {"type": "str", "choices": ["available", "installed"]},
+            "revert": {"type": "str", "choices": ["all", "latest"]},
+        },
+        required_one_of=[["apply", "list", "revert"]],
+        mutually_exclusive=[["apply", "list", "revert"]],
+        supports_check_mode=False,
     )
 
     result = {
-        'changed': False,
-        'command': '/usr/sbin/syspatch',
-        'msg': 'no action performed',
-        'rc': 0,
-        'stdout': '',
-        'stderr': '',
+        "changed": False,
+        "command": "/usr/sbin/syspatch",
+        "msg": "no action performed",
+        "rc": 0,
+        "stdout": "",
+        "stderr": "",
     }
 
-    Apply = module.params['apply']
-    List = module.params['list']
-    Revert = module.params['revert']
-
-    if Apply in ['yes', True]:
+    if module.params["apply"]:
         result = syspatch_apply(result, module)
+    elif module.params["list"]:
+        result = syspatch_list(result, module)
+    elif module.params["revert"]:
+        result = syspatch_revert(result, module)
 
-    if List in ['available', 'installed']:
-        result = syspatch_list(result, module, List)
-
-    if Revert in ['yes', True]:
-        result = syspatch_revert(result, module, Revert)
-
-    if result['rc'] > 0:
+    if result["rc"] > 0:
         module.fail_json(**result)
     else:
         module.exit_json(**result)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- roles/openbsd.sysupgrade/library/sysupgrade.py
+++ roles/openbsd.sysupgrade/library/sysupgrade.py
@@ -6,86 +6,72 @@
 # GNU General Public License v3.0+ (see COPYING or
 # https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from __future__ import (absolute_import)
+from __future__ import absolute_import
 from ansible.module_utils.basic import AnsibleModule
 
 
-def upgrade(r, module, Branch, Force):
-    if Branch == 'release':
-        r['command'] = "%s -r" % (r['command'])
-    elif Branch == 'snapshot':
-        r['command'] = "%s -s" % (r['command'])
+def upgrade(r, m):
+    if m.params["branch"] == "release":
+        r["command"] = "%s -r" % (r["command"])
 
-    if Force:
-        r['command'] = "%s -f" % (r['command'])
+    if m.params["branch"] == "snapshot":
+        r["command"] = "%s -s" % (r["command"])
 
-    r['rc'], r['stdout'], r['stderr'] = module.run_command(
-        r['command'], check_rc=False
-    )
+    if m.params["force"]:
+        r["command"] = "%s -f" % (r["command"])
 
-    if 'ftp: Error retrieving file: 404 Not Found' in r['stderr']:
+    r["rc"], r["stdout"], r["stderr"] = m.run_command(
+        r["command"], check_rc=False)
+
+    if "ftp: Error retrieving file: 404 Not Found" in r["stderr"]:
         # This signifies that no later release is available, which
         # isn't really an error.
-        r['rc'] = 0
-        r['msg'] = 'already on the latest available release'
-    elif r['rc'] > 0:
-        r['changed'] = False
-        r['msg'] = 'received a non-zero exit code'
-    elif 'Already on latest' in r['stdout']:
-        r['changed'] = False
-        r['msg'] = 'no action required'
-    elif 'Will upgrade on next reboot' in r['stdout']:
-        r['changed'] = True
-        r['msg'] = 'Upgrade prepared successfully'
+        r["rc"] = 0
+        r["msg"] = "already on the latest available release"
+    elif r["rc"] > 0:
+        r["changed"] = False
+        r["msg"] = "received a non-zero exit code"
+    elif "Already on latest" in r["stdout"]:
+        r["changed"] = False
+        r["msg"] = "no action required"
+    elif "Will upgrade on next reboot" in r["stdout"]:
+        r["changed"] = True
+        r["msg"] = "Upgrade prepared successfully"
     else:
-        r['changed'] = True
-        r['msg'] = "something unexpected happened"
-        r['rc'] = 1
+        r["changed"] = True
+        r["msg"] = "something unexpected happened"
+        r["rc"] = 1
 
     return r
 
 
 def main():
     module_args = {
-        'branch': {
-            'type': 'str',
-            'choices': ['auto', 'release', 'snapshot'],
-        },
-        'force': {
-            'type': 'bool',
-            'default': False,
-        },
+        "branch": {"type": "str", "choices": ["auto", "release", "snapshot"]},
+        "force": {"type": "bool", "default": False},
     }
 
     module = AnsibleModule(
         argument_spec=module_args,
-        required_one_of=[['branch']],
-        supports_check_mode=True,
+        supports_check_mode=True
     )
 
     result = {
-        'changed': False,
-        'command': '/usr/sbin/sysupgrade -n',
-        'msg': 'no action performed',
-        'rc': 0,
-        'stderr': '',
-        'stdout': '',
+        "changed": False,
+        "command": "/usr/sbin/sysupgrade -n",
+        "msg": "no action performed",
+        "rc": 0,
+        "stderr": "",
+        "stdout": "",
     }
 
-    Branch = module.params['branch']
-    Force = module.params['force']
+    result = upgrade(result, module)
 
-    if Force in ['yes', True]:
-        Force = True
-
-    if Branch in ['auto', 'release', 'snapshot']:
-        result = upgrade(result, module, Branch, Force)
-
-    if result['rc'] > 0:
+    if result["rc"] > 0:
         module.fail_json(**result)
     else:
         module.exit_json(**result)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
