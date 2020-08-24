23e68d6fd52e7d48f2ef6a25a72c2113cc206013
jcmdln <jcmdln@gmail.com>
2020-08-23 22:03:31 -0400

    Initial 'pkg' implementation

--- openbsd_run/playbook/modules/pkg.py
+++ openbsd_run/playbook/modules/pkg.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-# Copyright: (c) 2019, Johnathan C. Maudlin <jcmdln@gmail.com>
-#
-# GNU General Public License v3.0+ (see COPYING or
-# https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import
 from ansible.module_utils.basic import AnsibleModule
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 
 class Pkg:
@@ -16,43 +10,125 @@ class Pkg:
         self.module: AnsibleModule = module
 
         self.changed: bool = False
-        self.command: str = "/usr/sbin/pkg_add"
+        self.command: str = ""
         self.msg: str = ""
         self.rc: int = 0
-        self.reboot: bool = False
         self.stdout: str = ""
         self.stderr: str = ""
 
-    def Add(self) -> None:
+        self.host_packages: List[str] = []
+
+    def Add(self, packages: List[str]) -> None:
         """
         """
 
-    def Check(self) -> None:
+        package_action: str = "installed"
+        self.command = "/usr/sbin/pkg_add"
+
+        if self.module.params["state"] == "latest":
+            package_action = "updated"
+            self.command = "{} -u".format(self.command)
+
+        if self.module.params["name"] and self.module.params["name"] != "*":
+            for package in self.module.params["name"]:
+                self.command = "{} {}".format(self.command, package)
+
+        self.rc, self.stdout, self.stderr = self.module.run_command(
+            self.command, check_rc=False
+        )
+
+        if self.rc != 0:
+            self.msg = "received a non-zero exit code"
+            return
+
+        if len(self.stdout.split("\n")) <= 2:
+            self.msg = "no action performed"
+            return
+
+        self.changed = True
+        self.msg = "packages {}".format(package_action)
+
+    def Delete(self, packages: List[str]) -> None:
         """
         """
 
-    def Delete(self) -> None:
+        package_action: str = "removed"
+        self.command = "/usr/sbin/pkg_delete"
+
+        if self.module.params["name"] == "*":
+            self.command = "{} -a".format(self.command)
+        else:
+            for package in self.module.params["name"]:
+                self.command = "{} {}".format(self.command, package)
+
+        self.rc, self.stdout, self.stderr = self.module.run_command(
+            self.command, check_rc=False
+        )
+
+        if self.rc != 0:
+            self.msg = "received a non-zero exit code"
+            return
+
+        if len(self.stdout.split("\n")) <= 2:
+            self.msg = "no action performed"
+            return
+
+        self.changed = True
+        self.msg = "packages {}".format(package_action)
+
+    def Info(self) -> List[str]:
         """
         """
 
-    def Info(self) -> None:
-        """
-        """
+        self.command = "/usr/sbin/pkg_info -q"
+        self.rc, self.stdout, self.stderr = self.module.run_command(
+            self.command, check_rc=False
+        )
+
+        self.host_packages = self.stdout.split("\n")
+
+        if self.module.params["name"] and self.module.params["name"] != "*":
+            for package in self.module.params["name"]:
+                self.command = "{} -I inst:{}".format(self.command, package)
+
+            self.rc, self.stdout, self.stderr = self.module.run_command(
+                self.command, check_rc=False
+            )
+
+        return self.stdout.split("\n")
 
 
 def main() -> None:
     module: AnsibleModule = AnsibleModule(
         argument_spec={
-            "name": {"type": "str", "required": True},
+            "list": {"type": "bool"},
+            "name": {"elements": "str", "type": "list"},
             "state": {
-                "type": "str",
                 "choices": ["absent", "latest", "present"],
+                "required": True,
+                "type": "str",
             },
         },
+        mutually_exclusive=[["list", "name"], ["list", "state"]],
+        required_one_of=[["list", "name"]],
         supports_check_mode=True,
     )
 
     pkg: Pkg = Pkg(module)
+
+    if module.params["state"] in ["absent", "present"]:
+        pkg_list: List[str] = pkg.Info()
+
+    if module.params["state"] == "absent":
+        to_del: List[str] = list(set(module.params["name"]) - set(pkg_list))
+        if to_del:
+            pkg.Delete(to_del)
+    elif module.params["state"] == "present":
+        to_add: List[str] = list(set(pkg_list) - set(module.params["name"]))
+        if to_add:
+            pkg.Add(to_add)
+    elif module.params["name"] and module.params["state"] == "latest":
+        pkg.Add(module.params["name"])
 
     # Convert specific properties to a dict so we return specific data
     result: Dict[str, Any] = {
@@ -60,7 +136,6 @@ def main() -> None:
         "command": pkg.command,
         "msg": pkg.msg,
         "rc": pkg.rc,
-        "reboot": pkg.reboot,
         "stdout": pkg.stdout,
         "stderr": pkg.stderr,
     }
