7a3d7d2fdd4bc01d07e912e2b712e36663c3fe0f
jcmdln <jcmdln@gmail.com>
2020-08-29 03:07:07 -0400

    Initial work for adding/updating/deleting packages

--- openbsd_run/playbook/modules/pkg.py
+++ openbsd_run/playbook/modules/pkg.py
@@ -3,105 +3,125 @@
 from __future__ import absolute_import
 from ansible.module_utils.basic import AnsibleModule
 from typing import Any, Dict, List
+import re
 
 
 class Pkg:
     def __init__(self, module: AnsibleModule) -> None:
         self.module: AnsibleModule = module
+        self.packages: Dict[str, Any] = {}
+        self.packages_raw: List[str] = []
 
         self.changed: bool = False
         self.command: str = ""
-        self.msg: str = ""
+        self.msg: str = "no action performed"
         self.rc: int = 0
         self.stdout: str = ""
         self.stderr: str = ""
 
-        self.host_packages: List[str] = []
-
-    def Add(self, packages: List[str]) -> None:
+    def Add(self) -> None:
         """
+        Add or update package(s)
         """
 
-        package_action: str = "installed"
-        self.command = "/usr/sbin/pkg_add"
+        self.command = "/usr/sbin/pkg_add -Ivx"
+        pkgs: List[str] = self.module.params["name"]
+        new_pkgs: List[str] = list(
+            set(pkgs) - set(self.packages_raw) - set(self.packages.keys())
+        )
 
         if self.module.params["state"] == "latest":
-            package_action = "updated"
-            self.command = "{} -u".format(self.command)
+            pkg_action = "updated"
+            __command = "{} -u".format(self.command)
+            if "*" not in self.module.params["name"]:
+                for p in [p for p in self.packages.keys() if p in pkgs]:
+                    __command = "{} {}".format(self.command, p)
 
-        if self.module.params["name"] and self.module.params["name"] != "*":
-            for package in self.module.params["name"]:
-                self.command = "{} {}".format(self.command, package)
-
-        self.rc, self.stdout, self.stderr = self.module.run_command(
-            self.command, check_rc=False
-        )
-
-        if self.rc != 0:
-            self.msg = "received a non-zero exit code"
-            return
-
-        if len(self.stdout.split("\n")) <= 2:
-            self.msg = "no action performed"
-            return
-
-        self.changed = True
-        self.msg = "packages {}".format(package_action)
-
-    def Delete(self, packages: List[str]) -> None:
-        """
-        """
-
-        package_action: str = "removed"
-        self.command = "/usr/sbin/pkg_delete"
-
-        if self.module.params["name"] == "*":
-            self.command = "{} -a".format(self.command)
+            self.rc, self.stdout, self.stderr = self.module.run_command(
+                __command, check_rc=False, use_unsafe_shell=True
+            )
         else:
-            for package in self.module.params["name"]:
-                self.command = "{} {}".format(self.command, package)
+            pkg_action = "installed"
 
-        self.rc, self.stdout, self.stderr = self.module.run_command(
-            self.command, check_rc=False
-        )
-
-        if self.rc != 0:
-            self.msg = "received a non-zero exit code"
-            return
-
-        if len(self.stdout.split("\n")) <= 2:
-            self.msg = "no action performed"
-            return
-
-        self.changed = True
-        self.msg = "packages {}".format(package_action)
-
-    def Info(self) -> List[str]:
-        """
-        """
-
-        self.command = "/usr/sbin/pkg_info -q"
-        self.rc, self.stdout, self.stderr = self.module.run_command(
-            self.command, check_rc=False
-        )
-
-        self.host_packages = self.stdout.split("\n")
-
-        if self.module.params["name"] and self.module.params["name"] != "*":
-            for package in self.module.params["name"]:
-                self.command = "{} -I inst:{}".format(self.command, package)
+        if new_pkgs and "*" not in self.module.params["name"]:
+            for p in new_pkgs:
+                self.command = "{} {}".format(self.command, p)
 
             self.rc, self.stdout, self.stderr = self.module.run_command(
                 self.command, check_rc=False
             )
 
-        return self.stdout.split("\n")
+        if self.rc != 0:
+            self.msg = "received a non-zero exit code"
+            return
+
+        if len(self.stdout) <= 2:
+            self.command = ""
+            return
+
+        self.changed = True
+        self.msg = "packages {}".format(pkg_action)
+
+    def Delete(self) -> None:
+        """
+        Delete installed package(s)
+        """
+
+        self.command = "/usr/sbin/pkg_delete -Ivx"
+        to_delete: Dict[str, Any] = {}
+
+        if "*" in self.module.params["name"]:
+            self.command = ""
+            self.msg = "refusing to attempt removing all packages"
+            self.rc = 1
+            return
+
+        for pkg in self.packages:
+            if pkg in self.module.params["name"]:
+                to_delete[pkg] = ""
+
+        if not to_delete:
+            self.command = ""
+            self.msg = "no action performed"
+            return
+
+        for package in to_delete:
+            self.command = "{} {}".format(self.command, package)
+
+        self.rc, self.stdout, self.stderr = self.module.run_command(
+            self.command, check_rc=False
+        )
+
+        if self.rc != 0:
+            self.msg = "received a non-zero exit code"
+            return
+
+        self.changed = True
+        self.msg = "packages removed successfully"
+
+    def Info(self) -> None:
+        """
+        Collect information about currently installed packages
+        """
+
+        self.command = "/usr/sbin/pkg_info -q"
+        self.rc, stdout, stderr = self.module.run_command(
+            self.command, check_rc=False
+        )
+
+        pkgs = stdout.split("\n")
+        for pkg in pkgs:
+            if not pkg:
+                continue
+            name = re.sub(r"-[0-9].*$", "", pkg)
+            vers = re.search(r"-([\d.]+.*$)", pkg).group(1)  # type: ignore
+            self.packages[name] = {"version": "{}".format(vers)}
+            self.packages_raw.append(pkg)
 
 
 def main() -> None:
     module: AnsibleModule = AnsibleModule(
         argument_spec={
-            "list": {"type": "bool"},
             "name": {"elements": "str", "type": "list"},
             "state": {
                 "choices": ["absent", "latest", "present"],
@@ -116,19 +136,12 @@ def main() -> None:
 
     pkg: Pkg = Pkg(module)
 
-    if module.params["state"] in ["absent", "present"]:
-        pkg_list: List[str] = pkg.Info()
+    pkg.Info()
 
     if module.params["state"] == "absent":
-        to_del: List[str] = list(set(module.params["name"]) - set(pkg_list))
-        if to_del:
-            pkg.Delete(to_del)
-    elif module.params["state"] == "present":
-        to_add: List[str] = list(set(pkg_list) - set(module.params["name"]))
-        if to_add:
-            pkg.Add(to_add)
-    elif module.params["name"] and module.params["state"] == "latest":
-        pkg.Add(module.params["name"])
+        pkg.Delete()
+    elif module.params["state"] in ["latest", "present"]:
+        pkg.Add()
 
     # Convert specific properties to a dict so we return specific data
     result: Dict[str, Any] = {
